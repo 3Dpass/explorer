3faab106dd9b0c91c27dee17c2683c6e7390c08d
Mikhail Fedosov <tbs.micle@gmail.com>
2025-04-04 19:11:37 +0400

    refactor(home/explore): reorder imports and update title

--- site/src/components/home/explore/index.js
+++ site/src/components/home/explore/index.js
@@ -1,14 +1,14 @@
-import styled, { css } from "styled-components";
-import { Inter_24_700 } from "../../../styles/text";
-import { Button } from "../../styled/buttons";
-import { Flex } from "../../styled/flex";
+import { useRef } from "react";
 import { useSelector } from "react-redux";
+import styled, { css } from "styled-components";
 import { chainSettingSelector } from "../../../store/reducers/settingSlice";
 import { smcss } from "../../../styles/responsive";
-import ExploreInputOrigin from "./input";
-import { useRef } from "react";
 import { bg_theme500, w_full } from "../../../styles/tailwindcss";
+import { Inter_24_700 } from "../../../styles/text";
 import { useIsDark } from "../../../utils/hooks";
+import { Button } from "../../styled/buttons";
+import { Flex } from "../../styled/flex";
+import ExploreInputOrigin from "./input";
 
 const ExploreInput = styled(ExploreInputOrigin)`
   ${w_full};
@@ -53,7 +53,7 @@ export default function Explore() {
 
   return (
     <div>
-      <Title>{settings.name} Explorer</Title>
+      <Title>The Ledger of Things Explorer</Title>
       <Wrapper style={{ gap: 16 }}>
         <ExploreInput ref={exploreRef} />
         <ExploreButton dark={isDark} onClick={handleExplore}>
